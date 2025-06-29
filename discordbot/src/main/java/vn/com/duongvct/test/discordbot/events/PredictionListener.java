1c3c1dc3f28e6be5c98a7314fb3dfc8b1e5adefa
Duong Vu Cong Tuan <DuongVCT.B22KH024@stu.ptit.edu.vn>
2025-06-29 10:06:16 +0700

    update

---
+++ discordbot/src/main/java/vn/com/duongvct/test/discordbot/events/PredictionListener.java
@@ -0,0 +1,8 @@
+package vn.com.duongvct.test.discordbot.events;
+
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PredictionListener extends ListenerAdapter {
+}
